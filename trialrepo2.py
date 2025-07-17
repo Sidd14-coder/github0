42e6898ec936a10db0d4c70a6c79240e85c6ccdb
Siddhesh Chavan <savitachavan029@gmail.com>
2025-07-17 11:44:13 +0530

    first commit

--- trialrepo2.py
+++ trialrepo2.py
@@ -1,6 +1,6 @@
 x=3
 print(x)
 print("Chhatrapathi Shivaji Maharaj is our God")
-print("Jai Bhavani Jai Shivaji")
+print("First language of India is Marathi")
 print("Hello World, I am the one who gonna rule the world in future")
 print("Hello Bhai log")
